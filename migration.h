2eeb06b9f2af1d122dbfe4c32ce4e53e3ef8598e
Mitchell Werensteijn <mitchellwerensteijn@msn.com>
2020-12-29 14:47:29 +0100

    Prepares the arguments for calling CRIU

--- migration.h
+++ migration.h
@@ -13,26 +13,29 @@ void migrate_to_sw() {
 
     printf("parent_pid: %d\n", parent_pid);
 
-    // char *aargv[3];
-    // aargv[0] = "../criu.sh";
-    // aargv[1] = "dump -t -D check --shell-job #-v0;";
-    // aargv[2] = 0;
-
     pid_t pid = fork();
     switch (pid) {
         case -1: /* error */
             exit(-1);
         case 0:  /* child */
+        {
+            // Prepare the arguments for CRIU
+            char pid_buffer[10] = { };
+            snprintf(pid_buffer, 10, "%d", parent_pid);
+
+            char *args[]={"./criu.sh", "dump", "-t", pid_buffer, "-D", "check", "--shell-job", "-v4", NULL}; 
+
             // Detach from the parent process otherwise the child will be checkpointed as well
             setsid();
 
             // File descriptors are inherited from the parent even after exec, close them to be sure.
             for (int fd = 3; fd < 256; fd++) 
                 (void) close(fd);
-            
+
             printf("my pid: %d and the parent is: %d\n", getpid(), parent_pid);
             exit(0);
-            //execvp("../criu", aargv);
+            // execvp("./criu.sh", args);
+        }
     }
 
     sleep(2);
