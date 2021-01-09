764d95cdf73a0e779d56887fdabe73e09a98190c
Mitchell Werensteijn <mitchellwerensteijn@msn.com>
2021-01-09 20:03:11 +0100

    To test swapping in and out to SRAM

--- main.c
+++ main.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "trusted_cr.h"
 
+#define NUM_OF_PAGES 50 
+#define SMALL_PAGE_SIZE 4096
+
 int main() {
-	printf("Hello World\n");
-	
-	sleep(1);
-	
-	printf("This is a sample program\n");
+	printf("SRAM Swapping test\n");
 
-	// Migrate to the secure world
-	trusted_cr_migrate_to_sw();
+	int TEST_BUFFER_SIZE = NUM_OF_PAGES * SMALL_PAGE_SIZE;
+	char * test_buffer = malloc(TEST_BUFFER_SIZE);
 
-	printf("Intended to demo the binary\n");
-	sleep(1);
-	
-	char words[50];
-	scanf("%s", words); 
-	printf("%s\n", words);
-	
-	
-	// Migrate back to the normal world
-	trusted_cr_migrate_back_to_nw();
-
-	printf("We are briefly back in the normal world\n");
-
-	// Migrate to the secure world
-	trusted_cr_migrate_to_sw();
-	sleep(1);
-	
-	int y = 0;
-	
-	for(int y = 0; y < 300; y++) {
-		printf("all of this will run securely: %d\n", y);
+	if(!test_buffer) {
+		printf("Exit: Unable to allocate %d bytes\n", TEST_BUFFER_SIZE);
+		exit(1);
 	}
-	
-	sleep(1);
-	// Migrate back to the normal world
+
+	// Test touching multiple pages repeatedly assigning and reading from them causing them to get swapped in and out of SRAM
+	// Use this to measure pagefault performance  
+	trusted_cr_migrate_to_sw();
+	for(int x = 0; x < 10000; x++) {
+		for(int i = 0; i < TEST_BUFFER_SIZE; i += 4096) {
+			test_buffer[i] = test_buffer[TEST_BUFFER_SIZE-i];
+		}
+	}
 	trusted_cr_migrate_back_to_nw();
-	
-	printf("Bye\n");
-	
+
+	free(test_buffer);
+		
 	return 0;
 }
