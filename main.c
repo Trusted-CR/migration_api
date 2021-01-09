f27f41382edabc54e60877de7fef5b285ff91d87
Mitchell Werensteijn <mitchellwerensteijn@msn.com>
2021-01-09 21:22:56 +0100

    Can be used to test the performance of checkpoint pagefaults

--- main.c
+++ main.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "trusted_cr.h"
+#include "big_initialized_data.h"
 
-#define NUM_OF_PAGES 50 
-#define SMALL_PAGE_SIZE 4096
+extern char big_data_buffer[];
+
+struct timeval  start_time, end_time;
 
 int main() {
 	printf("SRAM Swapping test\n");
 
 	int TEST_BUFFER_SIZE = NUM_OF_PAGES * SMALL_PAGE_SIZE;
-	char * test_buffer = malloc(TEST_BUFFER_SIZE);
 
-	if(!test_buffer) {
-		printf("Exit: Unable to allocate %d bytes\n", TEST_BUFFER_SIZE);
-		exit(1);
+	int x = 0;
+	
+	gettimeofday(&start_time, NULL);
+	for(int i = 0; i < TEST_BUFFER_SIZE; i++) {
+		// Code optimization is disabled with -O0 to prevent code being optimized away.
+		x = big_data_buffer[i];
 	}
-
-	// Test touching multiple pages repeatedly assigning and reading from them causing them to get swapped in and out of SRAM
-	// Use this to measure pagefault performance  
+	gettimeofday(&end_time, NULL);
+	printf ("Preparing the shared buffers: Total time = %f seconds \n", 
+	(double) (end_time.tv_sec - start_time.tv_sec) + ((double) (end_time.tv_usec - start_time.tv_usec) / 1000000.0));
+	
+	x = 0;
 	trusted_cr_migrate_to_sw();
-	for(int x = 0; x < 10000; x++) {
-		for(int i = 0; i < TEST_BUFFER_SIZE; i += 4096) {
-			test_buffer[i] = test_buffer[TEST_BUFFER_SIZE-i];
-		}
+	for(int i = 0; i < TEST_BUFFER_SIZE; i++) {
+		// Code optimization is disabled with -O0 to prevent code being optimized away.
+		x = big_data_buffer[i];
 	}
 	trusted_cr_migrate_back_to_nw();
-
-	free(test_buffer);
-		
+	
 	return 0;
 }
