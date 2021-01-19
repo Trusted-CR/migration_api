a4462dacacb66923d270566159cf7dde039cfdd6
Mitchell Werensteijn <mitchellwerensteijn@msn.com>
2021-01-19 15:52:51 +0100

    vfp faults

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
-/opt/optee-qemu/toolchains/aarch64/bin/aarch64-linux-gnu-gcc main.c trusted_cr.c -o main -static -Wl,-Ttext-segment,0x40050000
-cp main /opt/optee-qemu/build/shared_folder/main
+/opt/optee-qemu/toolchains/aarch64/bin/aarch64-linux-gnu-gcc -O0  main.c trusted_cr.c -o main -static -Wl,-Ttext-segment,0x40050000
+cp -rf main /opt/optee-qemu/build/shared_folder/main
+cp -rf main /opt/optee-hikey/out-br/target/root/shared/main
--- main.c
+++ main.c
@@ -2,30 +2,82 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <time.h>
+
 #include "trusted_cr.h"
 
 #define NUM_OF_PAGES 50 
 #define SMALL_PAGE_SIZE 4096
 
+#define SEC_TO_NANOSEC 1000000000
+
 int main() {
 	printf("SRAM Swapping test\n");
 
+	u_int64_t vregs[64];
+	unsigned long long starttime;
+	unsigned long long stoptime;
+
+	unsigned long long elapsedtime;
+
 	int TEST_BUFFER_SIZE = NUM_OF_PAGES * SMALL_PAGE_SIZE;
-	char * test_buffer = malloc(TEST_BUFFER_SIZE);
+	char * test_buffer = (char*) malloc(TEST_BUFFER_SIZE);
 
 	if(!test_buffer) {
 		printf("Exit: Unable to allocate %d bytes\n", TEST_BUFFER_SIZE);
 		exit(1);
 	}
 
+	struct timespec start, end;
+
 	// Test touching multiple pages repeatedly assigning and reading from them causing them to get swapped in and out of SRAM
 	// Use this to measure pagefault performance  
 	trusted_cr_migrate_to_sw();
-	for(int x = 0; x < 10000; x++) {
-		for(int i = 0; i < TEST_BUFFER_SIZE; i += 4096) {
-			test_buffer[i] = test_buffer[TEST_BUFFER_SIZE-i];
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	for(int x = 0; x < 1000; x++) {
+		for(int i = 0; i < TEST_BUFFER_SIZE; i++) {
+			test_buffer[i] = 1;
 		}
 	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	starttime = start.tv_sec * SEC_TO_NANOSEC + start.tv_nsec;
+	stoptime = end.tv_sec * SEC_TO_NANOSEC + end.tv_nsec;
+
+	elapsedtime = stoptime - starttime;	
+	printf("Took: %d seconds and %d nsec\n", elapsedtime / SEC_TO_NANOSEC,  elapsedtime % SEC_TO_NANOSEC);
+	
+	int p;
+	u_int32_t mvfr0;
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	for(int x = 0; x < 1000; x++) {
+		for(int i = 0; i < TEST_BUFFER_SIZE; i++) {
+			test_buffer[i] = 1;
+			// test_buffer[i] = test_buffer[TEST_BUFFER_SIZE-i];
+			__asm__ volatile("FADD	S0, S1, S2");
+		}
+	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	starttime = start.tv_sec * SEC_TO_NANOSEC + start.tv_nsec;
+	stoptime = end.tv_sec * SEC_TO_NANOSEC + end.tv_nsec;
+
+	elapsedtime = stoptime - starttime;	
+	printf("Took: %d seconds and %d nsec\n", elapsedtime / SEC_TO_NANOSEC,  elapsedtime % SEC_TO_NANOSEC);
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	for(int x = 0; x < 1000; x++) {
+		for(int i = 0; i < TEST_BUFFER_SIZE; i++) {
+			p = test_buffer[i];
+		}
+	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	starttime = start.tv_sec * SEC_TO_NANOSEC + start.tv_nsec;
+	stoptime = end.tv_sec * SEC_TO_NANOSEC + end.tv_nsec;
+
+	elapsedtime = stoptime - starttime;	
+	printf("Took: %d seconds and %d nsec\n", elapsedtime / SEC_TO_NANOSEC,  elapsedtime % SEC_TO_NANOSEC);
+
+
+
 	trusted_cr_migrate_back_to_nw();
 
 	free(test_buffer);
